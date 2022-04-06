6911b2cc86f34d95de719dad4d9ae7725d010d5d
Bemourn <70115233+koenib@users.noreply.github.com>
2022-04-06 15:23:55 -0600

    rollback to react 17.0.1

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,24 @@
 import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (<html lang="en">
-<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">NamePass</a>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/signup">Sign up</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/login">Sign in</a>
-        </li>
-      </ul>
+  return (
+    <div className="card bg-white card-rounded w-50">
+      <div className="card-header bg-dark text-center">
+        <h1>Welcome to MoonBay!</h1>
+      </div>
+      <div className="card-body m-5">
+        <h2>Hottest NFT collection right now:</h2>
+          <ul className="square">
+            <img src = "https://via.placeholder.com/350x150" alt="Placeholder" />
+          </ul>
+      </div>
+      <div className="card-footer text-center m-3">
+        <h2>Ready to explore?</h2>
+        <Link to="/explore">
+          <button className="btn btn-lg btn-danger">Explore!</button>
+        </Link>
+      </div>
     </div>
-  </div>
-</nav>
-
-<body>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"></script>
-</body>
-<footer class="fixed-bottom text-center text-white" style="background-color: #f1f1f1;">
-  <div class="text-center text-light p-3 pb-6" style="background-color: black;">
-    © 2022
-    <a class="text-white" href="https://mynamepass.com/">NamePassPro</a>
-  </div>
-</footer>
-
-</html>
   );
 };
 
