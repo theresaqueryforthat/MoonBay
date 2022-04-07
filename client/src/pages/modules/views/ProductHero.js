a9e5ea6287ab587b0eb72feedb15406a29fe68f8
Rick Ansay <rick.ansay@gmail.com>
2022-04-06 21:38:09 -0600

    Created homepage layout

---
+++ client/src/pages/modules/views/ProductHero.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Button from '../components/Button';
+import Typography from '../components/Typography';
+import ProductHeroLayout from './ProductHeroLayout';
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1534447677768-be436bb09401';
+
+export default function ProductHero() {
+  return (
+    <ProductHeroLayout
+      sxBackground={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundColor: '#7fc7d9', // Average color of the background image.
+        backgroundPosition: 'center',
+      }}
+    >
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Today's Hottest Collections
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        sx={{ mb: 4, mt: { sx: 4, sm: 10 } }}
+      >
+        Find, fave, and buy all your best NFTs at MoonBay
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+        sx={{ minWidth: 200 }}
+      >
+        Register
+      </Button>
+      <Typography variant="body2" color="inherit" sx={{ mt: 2 }}>
+        Discover the experience
+      </Typography>
+    </ProductHeroLayout>
+  );
+}
